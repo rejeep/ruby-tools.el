0306ee5920cd836111a74c62cdb257cec253ef48
Johan Andersson <johan.rejeep@gmail.com>
2012-03-27 21:11:44 +0200

    Add skeleton structure to Elisp file.

---
+++ ruby-tools.el
@@ -0,0 +1,66 @@
+;;; ruby-tools.el --- Collection of handy functions for ruby-mode
+
+;; Copyright (C) 2012 Johan Andersson
+
+;; Author: Johan Andersson <johan.rejeep@gmail.com>
+;; Maintainer: Johan Andersson <johan.rejeep@gmail.com>
+;; Version: 0.0.1
+;; Keywords: speed, convenience, ruby
+;; URL: http://github.com/rejeep/ruby-tools
+
+;; This file is NOT part of GNU Emacs.
+
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+
+;;; Commentary:
+
+;; ruby-tools is a minor mode for Emacs that can be used with
+;; ruby-mode. It includes various handy functions that will improve
+;; your productivity.
+;;
+;; To use ruby-tools-mode, make sure that this file is in Emacs load-path:
+;;   (add-to-list 'load-path "/path/to/directory/or/file")
+;;
+;; Then require ruby-tools:
+;;   (require 'ruby-tools)
+;;
+;; ruby-tools-mode is automatically started in ruby-mode.
+
+
+;;; Code:
+
+(defvar ruby-tools-mode-map
+  (let ((map (make-sparse-keymap)))
+    
+    map)
+  "Keymap for `ruby-tools-mode'.")
+
+;;;###autoload
+(define-minor-mode ruby-tools-mode
+  "Collection of handy functions for ruby-mode."
+  :init-value nil
+  :lighter " rt"
+  :keymap ruby-tools-mode-map)
+
+(add-hook 'ruby-mode-hook 'ruby-tools-mode)
+
+(provide 'ruby-tools)
+
+;;; ruby-tools.el ends here
