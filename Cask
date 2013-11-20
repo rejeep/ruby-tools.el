1d486562966da518247f686268cf254eea8f2185
Johan Andersson <johan.rejeep@gmail.com>
2013-11-20 15:08:57 +0100

    Use cl-lib for assert.

--- Cask
+++ Cask
@@ -1,3 +1,4 @@
+(source gnu)
 (source melpa)
 
 (package "ruby-tools" "0.1.0" "Collection of handy functions for ruby-mode.")
--- features/step-definitions/ruby-tools-steps.el
+++ features/step-definitions/ruby-tools-steps.el
@@ -1,20 +1,22 @@
+(require 'cl-lib)
+
 (Then "^ruby-tools-mode should be active$"
       (lambda ()
-        (assert ruby-tools-mode nil "Expected `ruby-tools-mode' to be started, but was not.")))
+        (cl-assert ruby-tools-mode nil "Expected `ruby-tools-mode' to be started, but was not.")))
 
 (When "^I go to character \"\\(.+\\)\"$"
       (lambda (char)
         (goto-char (point-min))
         (let ((search (re-search-forward (format "%s" char) nil t))
               (message "Can not go to character '%s' since it does not exist in the current buffer: %s"))
-          (assert search nil message char (espuds-buffer-contents)))))
+          (cl-assert search nil message char (espuds-buffer-contents)))))
 
 (When "^I go to the \\(front\\|back\\) of character \"\\(.+\\)\"$"
       (lambda (position char)
         (goto-char (point-min))
         (let ((search (re-search-forward (format "%s" char) nil t))
               (message "Can not go to character '%s' since it does not exist in the current buffer: %s"))
-          (assert search nil message char (espuds-buffer-contents))
+          (cl-assert search nil message char (espuds-buffer-contents))
           (cond
            ((string-equal "front" position) (backward-char))
            ((string-equal "back" position) (forward-char))))))
@@ -25,5 +27,5 @@
         (goto-char (point-min))
         (let ((search (re-search-forward (format "%s" word) nil t))
               (message "Can not go to character '%s' since it does not exist in the current buffer: %s"))
-          (assert search nil message word (espuds-buffer-contents))
+          (cl-assert search nil message word (espuds-buffer-contents))
           (if (string-equal "front" pos) (backward-word)))))
